2dca17396b5c9ad38b336d4bbce51d73b70bb9b4
ASUS <julie.joy@live.vu.edu.au>
2022-03-04 18:42:45 +0530

    pro1

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# Python Program to find Area of a Rectangle
+
 
 width = float(input('Please Enter the Width of a Rectangle: '))
 height = float(input('Please Enter the Height of a Rectangle: '))
+
+Area = width * height
+
+print("\n Area of a Rectangle is:%.2f" %Area)
 
---
+++ new 1.py
@@ -0,0 +1,5 @@
+
+
+name = "Julie"
+print("Hello", name)
+
